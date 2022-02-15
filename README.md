d1c5833d1284fd50dfb91ea7fac63e75f0a5dbae
Donghyun KIM <andy5153a@gmail.com>
2022-02-15 00:48:23 +0000

    add solution

---
+++ README.md
@@ -0,0 +1,33 @@
+# Google HashCode 2022 Practice Round: One Pizza
+My solution for Google HashCode 2022 Practice Round
+
+## Problem
+You can find the problem statements [here](https://codingcompetitions.withgoogle.com/hashcode/round/00000000008f5ca9/00000000008f6f33).
+
+## Solution
+
+The codes for the solution are in [Solve.ipynb](notebooks/Solve.ipynb) and [src.cpp](src.cpp).
+
+The solution follows these steps:
+
+1. Processing ([Solve.ipynb](notebooks/Solve.ipynb))
+
+   1. Count ```likes```, ```dislikes``` and calculate ```like - dislike``` for each ingredient.
+   
+   2. Include the ingredients greater than ```0``` in 1.
+   
+2. Optimisation ([src.cpp](src.cpp))
+
+   1. Random swap for 30 min
+
+
+## Results
+| File  | Score |
+| ------------- | ------------- |
+| A - An example | 2 |
+| B - Basic  | 5 |
+| C - Coarse  | 5 |
+| D - Difficult | 1,804 |
+| E - Elaborate | 1,872 |
+| **Total** | **3,688** |
+With more optimisation, D and E might be improved. 
---
+++ src.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <random>
+#include <iterator>
+
+using namespace std;
+
+string IN[6] = { "inputs/a_an_example.in.txt", "inputs/b_basic.in.txt", "inputs/c_coarse.in.txt", "inputs/d_difficult.in.txt", "inputs/e_elaborate.in.txt" };
+string OUT[6] = { "outputs/a_an_example.in.txt", "outputs/b_basic.in.txt", "outputs/c_coarse.in.txt", "outputs/d_difficult.in.txt", "outputs/e_elaborate.in.txt" };
+string OUT_OPT[6] = { "outputs/a_an_example.opt.txt", "outputs/b_basic.opt.txt", "outputs/c_coarse.opt.txt", "outputs/d_difficult.opt.txt", "outputs/e_elaborate.opt.txt" };
+
+int C = 4;
+
+vector<vector<string>> likes;
+vector<vector<string>> dislikes;
+set<string> all;
+int N;
+
+int score(set<string>& ingreds) {
+    int s = 0;
+    for(int i=0;i<N;i++) {
+        bool fail = false;
+        for(auto like: likes[i]) {
+            if (ingreds.find(like) == ingreds.end()) {
+            //if (find(ingreds.begin(), ingreds.end(), like) == ingreds.end()) {
+                fail = true;
+                break;
+            }
+        }
+        if (!fail) {
+            for(auto dislike: dislikes[i]) {
+                if (ingreds.find(dislike) != ingreds.end()) {
+                //if(find(ingreds.begin(), ingreds.end(), dislike) != ingreds.end()) {
+                    fail = true;
+                    break;
+                }
+            }
+        }
+        if (!fail)
+            s++;
+    }
+    return s;
+}
+
+int main() {
+    freopen(IN[C].c_str(), "r", stdin);
+
+    string buf;
+    cin >> N;
+    likes.assign(N, vector<string>());
+    dislikes.assign(N, vector<string>());
+    int t;
+    for(int i=0;i<N;i++) {
+        cin >> t;
+        for(int j=0;j<t;j++) {
+            cin >> buf;
+            likes[i].push_back(buf);
+            all.insert(buf);
+        }
+        cin >> t;
+        for(int j=0;j<t;j++) {
+            cin >> buf;
+            dislikes[i].push_back(buf);
+            all.insert(buf);
+        }
+    }
+
+    freopen(OUT[C].c_str(), "r", stdin);
+    int Nout;
+    cin >> Nout;
+    set<string> ingreds;
+    for(int i=0;i<Nout;i++) {
+        cin >> buf;
+        ingreds.insert(buf);
+    }
+
+    vector<string> allv(all.begin(), all.end());
+
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, allv.size()-1);
+    
+    int maxScore = score(ingreds);
+    for(int C=1; C<=3; C++) {
+        for(int i=0; i<20000; i++) {
+            auto tmpIng = ingreds;
+            for(int c=0;c<C;c++) {
+                int k = dis(gen);            
+                if (tmpIng.find(allv[k]) == tmpIng.end())
+                    tmpIng.insert(allv[k]);
+                else
+                    tmpIng.erase(allv[k]);
+            }
+            int tmpScore = score(tmpIng);
+            if (tmpScore >= maxScore) {
+                maxScore = tmpScore;
+                ingreds = tmpIng;
+            }
+
+            if( i%1000 == 0) {
+                cout << C << " " << maxScore << " " << i << endl;
+            }
+        }
+    }
+    cout << "-----" << endl;
+    cout << score(ingreds) << endl;
+
+    freopen(OUT_OPT[C].c_str(), "w", stdout);
+    cout << ingreds.size();
+    for(auto k: ingreds) {
+        cout << " " << k;
+    }
+
+    return 0;
+
+}
